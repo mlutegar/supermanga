dc85ec7d70b1dc098a86ed37d9d281e8ce2b3f4d
mlutegar <mlutegar@gmail.com>
2022-08-08 23:16:48 -0300

    feat: adicionando css na página login

--- login.php
+++ login.php
@@ -4,32 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width ,initial-scale=1">
-    <script src="https://kit.fontawesome.com/7c9e86ad48.js" crossorigin="anonymous"></script>    
+    <script src="https://kit.fontawesome.com/7c9e86ad48.js" crossorigin="anonymous"></script>  
     <title>Login</title>
+    <link rel="stylesheet" href="css/login.css">
 </head>
 
-<body> <?php include("navbar.php")?>
+<body> 
+    <?php include("navbar.php")?>
 
     <div class="banner"></div>
 
-    <div class="rect">
-        <div>
-            <h2>LOGIN</h2>
-        </div>
-
-        <div>
-            <form action="loginSistema.php" method="post" class="loginform">
-                <label for="email">Email: </label>
-                <input type="email" name="email" id="email">
-        
-                <label for="password">Senha: </label>
-                <input type="password" name="password" id="password">
-        
-                <button type="submit">Enviar</button>
-            </form>
-        </div>
-    </div>
-
+    <main>
+        <div id="form-rect">
+            <div class="login">
+                <form action="loginSistema.php" method="post" class="loginform">
+                    <label for="email">Email </label><br>
+                    <input type="email" name="email" id="email" placeholder="Informe o seu e-mail">
+                    <br>
+                    <label for="password">Senha </label><br>
+                    <input type="password" name="password" id="password" placeholder="Informe a sua senha">
+                    <br>
+                    <button class="submit-btn" type="submit">Acessar</button>
+                </form>
+                <a href="createAccount.php"><button class="submit-btn">Criar conta</button></a>
+            </div>
+    </main>
 
 <?php include('footer.php') ?></body>
 </html>
