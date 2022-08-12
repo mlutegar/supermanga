f8a830bd4ee8fce238c4ce39bc3aaa0db093a705
mlutegar <mlutegar@gmail.com>
2022-08-11 22:27:00 -0300

    fix: correção do css do navbar

--- navbar.php
+++ navbar.php
@@ -4,51 +4,60 @@
 
 <header>
 
-    <div class="nav" id="navlogo">
-        <a class="logo" href="index.php"><img src="img/logo.png" alt="logo"> </a>
-    </div>
+    <div class="navbar">
 
-    <nav class="nav" id="navmenu">
-        <ul class="nav__links">
-            <li><a href="mangas.php">Mangás</a></li>
-            <li><a href="#">Categorias</a></li>
-            <li><a href="message_us.php">Fale conosco</a></li>
-        </ul>
-    </nav>
+        <div class="margin"></div>
 
-    <div class="nav" id="navsearch">
-        
-        <div class="search-box">
-            <form id="formSearchTitulo" role="search" method="post" action="localiza-manga.php">
-                <input id="searchTitulo" class="search-txt" type="search" name="titulo" placeholder="Faça sua pesquisa" aria-label="Search">
-            </form>
-            <a class="search-btn" href="#">
-                <img src="img/lupa.png" alt="" width="25" height="25">
-            </a>
-        </div>  
-    </div>
+        <div class="logo-box">
+            <a class="logo" href="index.php"><img src="img/logo.png" alt="logo"> </a>
+        </div>
 
-    <div id="nav-account">
+        <div class="links-nav">
+            <ul class="links">
+                <li><a href="mangas.php">Mangás</a></li>
+                <li><a href="#">Categorias</a></li>
+                <li><a href="message_us.php">Fale conosco</a></li>
+            </ul>
+        </div>
 
-        <?php if(empty(isset($_SESSION['login']))) : ?>
-            <a class="cta" href="login.php">Login</a>
-        <?php else: ?>
-            <div class="dropdown">
-                <a href="#"><img class="iconlogin" src="img/login.png" alt="" width="30" height="30"></a>
-
-                <div class="dropdown-content">
-                    <ul>
-                        <li><a class="logout" href="logout.php">Logout</a></li>
-                        <li><a class="logout" href="create_manga.php">Cadastrar manga</a></li>   
-                        <li><a class="logout" href="user.php">Páginar usuario</a></li>
-                        <?php if($_SESSION['login']->id == 1 && isset($_SESSION['login'])) echo  "<li><a class='logout' href='admin.php'>Administrador</a></li>" ?>                                              
-                    </ul>
-                </div>
+        <div class="search">
+            <div class="box-search">
+                <form id="formSearchTitulo" role="search" method="post" action="localiza-manga.php">
+                    <input id="searchTitulo" class="search-txt" type="search" name="titulo" placeholder="Faça sua pesquisa" aria-label="Search">
+                </form>
             </div>
-        <?php endif ?>
+            <div class="btn-search">
+                <div class="lupa">
+                    <a href="#">    
+                        <img src="img/lupa.png" alt="" width="25" height="25">
+                    </a>
+                </div>    
+            </div>
+        </div>
+
+        <div class="account">
+            <?php if(empty(isset($_SESSION['login']))) : ?>
+                <a class="cta" href="login.php">Login</a>
+            <?php else: ?>
+                <div class="dropdown">
+                    <a href="#"><img class="iconlogin" src="img/login.png" alt="" width="30" height="30"></a>
+
+                    <div class="dropdown-content">
+                        <ul>
+                            <li><a class="logout" href="logout.php">Logout</a></li>
+                            <li><a class="logout" href="create_manga.php">Cadastrar manga</a></li>   
+                            <li><a class="logout" href="user.php">Páginar usuario</a></li>
+                            <?php if($_SESSION['login']->id == 1 && isset($_SESSION['login'])) echo  "<li><a class='logout' href='admin.php'>Administrador</a></li>" ?>                                              
+                        </ul>
+                    </div>
+                </div>
+                <?php endif ?>
+        </div>
+
+        <div class="margin"></div>
+    
     </div>
 
-    <!--<p class="menu cta">Menu</p>-->
 
 </header>
 
