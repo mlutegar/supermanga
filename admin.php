a64a72d9b8f3f28e56024fee3de91c94dfff795a
mlutegar <mlutegar@gmail.com>
2022-08-12 22:39:08 -0300

    feat: add admin page

---
+++ admin.php
@@ -0,0 +1,22 @@
+<?php 
+    include('configAdmin.php');
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+</head>
+<body> <?php include('navbar.php'); ?>
+
+    <div class="banner">
+    </div>
+
+    <div style="text-align:center; margin-top:5%;">
+        <p><a href="create_manga.php">ADICIONAR</a> | <a href="admin_list.php">LISTAGEM DETALHADA</a></p>
+    </div>
+    
+</body> <?php include("footer.php"); ?>
+</html>
--- create_manga.php
+++ create_manga.php
@@ -18,32 +18,26 @@
               <div style="width: 100%;">
                 <img src="img/img1.png" id="manga-cover">
               </div>
+                <label>Capa <input class="upload-btn" type="file" name="capa" id="capaId" required></label>
+                <label>Manga<input class="upload-btn" type="file" name="conteudo" id="conteudoId" required></label>  
 
-                    <label>Capa <input class="upload-btn" type="file" name="capa" id="capaId" required></label>
-                   <label>Manga<input class="upload-btn" type="file" name="conteudo" id="conteudoId" required></label>
+                <label>Anime</label>
+                <input type="text" name="anime" id="animeId" required>
+
+                <label>Volume</label>
+                <input type="number" name="volume" id="volumeId" required>  
+
+                <label>Autor</label>
+                <input type="text" name="autor" id="autorId">            
 
       
+                <label>Categoria</label>
+                <input type="text" name="categoria" id="editoraId">
 
+                <label>Sumário <textarea style="height: 300px;" name="sumario" rows="8" id="sumarioId"></textarea></label>
 
-                      <label>Anime</label>
-                      <input type="text" name="anime" id="animeId" required>
-
-
-
-                    <label>Volume</label>
-                    <input type="number" name="volume" id="volumeId" required>  
-
-                      <label>Autor</label>
-                      <input type="text" name="autor" id="autorId">            
-
-        
-                    <label>Categoria</label>
-                    <input type="text" name="categoria" id="editoraId">
-
-                  <label>Sumário <textarea style="height: 300px;" name="sumario" rows="8" id="sumarioId"></textarea></label>
-
-                  <input class="submit-btn" type="submit" value="Enviar">
-                  <div id="notify"><?= isset($_COOKIE['notify']) ? $_COOKIE['notify'] : '' ?></div>
+                <input class="submit-btn" type="submit" value="Enviar">
+                <div id="notify"><?= isset($_COOKIE['notify']) ? $_COOKIE['notify'] : '' ?></div>
           </form>
         </div>
     </div>
